c84cb5863294bacc3247e35f12d4126027ce6433
rugger-ai <149358328+rugger-ai@users.noreply.github.com>
2023-11-15 18:06:57 -0800

    Added different modes (rag, chat, code)

--- LocalRAG.py
+++ LocalRAG.py
@@ -22,10 +22,14 @@ load_dotenv()
 embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME")
 persist_directory = os.environ.get('PERSIST_DIRECTORY')
 model_type = os.environ.get('MODEL_TYPE')
-model_path = os.environ.get('MODEL_PATH')
+model_path_RAG = os.environ.get('MODEL_PATH_RAG')
+model_path_chat = os.environ.get('MODEL_PATH_CHAT')
+model_path_code = os.environ.get('MODEL_PATH_CODE')
 model_n_ctx = os.environ.get('MODEL_N_CTX')
 model_n_ctx = os.environ.get('MODEL_N_CTX')
-model_temperature = os.environ.get('MODEL_TEMPERATURE')
+model_temperature_RAG = os.environ.get('MODEL_TEMPERATURE_RAG')
+model_temperature_chat = os.environ.get('MODEL_TEMPERATURE_CHAT')
+model_temperature_code = os.environ.get('MODEL_TEMPERATURE_CODE')
 max_tokens_generated = os.environ.get('MAX_TOKENS_GENERATED')
 is_gpu_enabled = (os.environ.get('IS_GPU_ENABLED', 'False').lower() == 'true')
 target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
@@ -50,19 +54,10 @@ def calculate_layer_count() -> int | None:
     else:
         return (get_gpu_memory()//LAYER_SIZE_MB-LAYERS_TO_REDUCE)
 
-# Create embeddings
-print("Loading Embeddings model....")
-embeddings = Embeddings(hybrid=True, path="sentence-transformers/nli-mpnet-base-v2")
+mode = input("Enter a mode ('rag', 'chat', 'code'): ")
 
-#load the embeddings from saved embeddings
-print("Loading Embeddings...")
-embeddings.load("db")
-
-# Instantiate the LLM
-llamallm = LlamaCpp(model_path=model_path, max_tokens = max_tokens_generated, temperature = model_temperature, n_ctx = model_n_ctx, verbose=False, n_gpu_layers=15)
-
-# Create the prompt template for the LLM
-prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+if mode == "rag":
+    prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
     {context}
 
@@ -70,42 +65,75 @@ prompt_template = """Use the following pieces of context to answer the question 
     Answer:
     """
 
-PROMPT = PromptTemplate(
-    template=prompt_template, input_variables=["context", "question"]
-)
+    llamallm = LlamaCpp(model_path=model_path_RAG, streaming=True, max_tokens = max_tokens_generated, temperature = model_temperature_RAG, n_ctx = model_n_ctx, verbose=False, n_gpu_layers=calculate_layer_count())
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+    print("Loading Embeddings model....")
+    embeddings = Embeddings(hybrid=True, path="sentence-transformers/nli-mpnet-base-v2")
 
-def talk_to_LLM(question, history):
+    #load the embeddings from saved embeddings
+    print("Loading Embeddings...")
+    embeddings.load("db")
+
+if mode == "chat":
+    prompt_template = """You are a helpful virtual assistant. 
+
+    Question: {question}
+    Answer:
+    """
+    llamallm = LlamaCpp(model_path=model_path_chat,  streaming=True, max_tokens = max_tokens_generated, temperature = model_temperature_chat, n_ctx = model_n_ctx, verbose=False, n_gpu_layers=calculate_layer_count())
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["question"]
+    )
+
+if mode == "code":
+    prompt_template = """You are a helpful virtual assistant for programmers. 
+
+    Question: {question}
+    Answer:
+    """
+    llamallm = LlamaCpp(model_path=model_path_code,  streaming=True, max_tokens = max_tokens_generated, temperature = model_temperature_code, n_ctx = model_n_ctx, verbose=False, n_gpu_layers=calculate_layer_count())
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["question"]
+    )
+
+
+while True:
+    question = input("Enter a query: ")
+
     start_time = time.time()
-    # Extract uid of first result
-    # search result format: (uid, score)
-    # return the top N results
-    connection = sqlite3.connect("db/embeddings.db")
-    cursor = connection.cursor()
-
-
-    uid = embeddings.search(question, target_source_chunks)
-
-    embeddingSearchResults = ""
     
-    for uid, score in uid:
-        #print("  %s" % data[uid])
-        cursor.execute(
-        'SELECT * FROM chunks LIMIT 1 offset (?)', (uid,))
-        results = cursor.fetchall()
-        embeddingSearchResults += "\n\n" + str(results)
+    if question != "" and len(question) > 1:
+        # Extract uid of first result
+        # search result format: (uid, score)
+        # return the top N results
+        if mode == 'rag':
+            connection = sqlite3.connect("db/embeddings.db")
+            cursor = connection.cursor()
+            uid = embeddings.search(question, target_source_chunks)
+
+            embeddingSearchResults = ""
+            
+            for uid, score in uid:
+                #print("  %s" % data[uid])
+                cursor.execute(
+                'SELECT * FROM chunks LIMIT 1 offset (?)', (uid,))
+                results = cursor.fetchall()
+                embeddingSearchResults += "\n\n" + str(results)
+
+            print("Embeddings Search Results:")
+            #embeddingSearchResults = embeddingSearchResults.replace(r'\n', '\n')
+            print(embeddingSearchResults)
 
 
-    print("Embeddings Search Results:")
-    #embeddingSearchResults = embeddingSearchResults.replace(r'\n', '\n')
-    print(embeddingSearchResults)
-    
-    if question != "":
         llmChain = LLMChain(llm=llamallm, prompt=PROMPT)
-        result = llmChain.run(question=question, context=embeddingSearchResults)
+        if mode == "rag":
+            result = llmChain.run(question=question, context=embeddingSearchResults)
+        else: 
+            result = llmChain.run(question=question)
         run_time = time.time() - start_time
-        print("Time to execute: " + str(run_time))
-        return result
-        #print("\n\nQuestion: " + question + "\n\nAnswer: " + llmChain.run(question=question, context=embeddingSearchResults)  + "\n\n")
+        print("Time to execute: " + str(round(run_time)) + " seconds")
+        print(result)
     
 
-gr.ChatInterface(talk_to_LLM).launch()
